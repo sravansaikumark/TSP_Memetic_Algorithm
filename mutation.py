53324c69976d66eb9310fe78359b068ff479d262
sravansaikumark <sravansaikumark@gmail.com>
2025-10-08 23:07:45 +0200

    Update mutation.py
    inverse mutation

--- mutation.py
+++ mutation.py
@@ -3,5 +3,6 @@ import random
 def mutate_swap(ind, mutation_rate=0.2):
     if random.random() < mutation_rate:
         i, j = random.sample(range(len(ind)), 2)
-        ind[i], ind[j] = ind[j], ind[i]
+        individual[i:j+1] = individual[i:j+1][::-1]
     return ind
+
